dc7684dc4cb8ebe6ec6304297659dde7995247ea
Wilson Huang <hjn201@gmail.com>
2015-12-13 22:34:57 +0800

    2015/12/13 10:34

--- app/src/main/java/com/motorcycle/motorcycleassistant/MainActivity.java
+++ app/src/main/java/com/motorcycle/motorcycleassistant/MainActivity.java
@@ -1,14 +1,14 @@
 package com.motorcycle.motorcycleassistant;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_menu);
     }
 }
 //
